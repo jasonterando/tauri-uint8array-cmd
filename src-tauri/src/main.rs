604056029f0d72ee801d0f7d264a06ce7644acd7
Jason Terando <jason.t@digitalroominc.com>
2024-07-23 07:38:16 -0500

    Added test for checking existence of /home/jason/.ssh

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![convert])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { invoke } from "@tauri-apps/api/core";
+import { homeDir, join } from '@tauri-apps/api/path'
+import { exists } from "@tauri-apps/plugin-fs"
 
 const runTest = async (useNumberArray: boolean, resultEl: HTMLElement) => {
   let value = useNumberArray ? {
@@ -16,10 +18,26 @@ const runTest = async (useNumberArray: boolean, resultEl: HTMLElement) => {
   }
 }
 
+const runSshTest = async (resultEl: HTMLElement) => {
+  const home = await homeDir()
+  const sshPath = await join(home, '.ssh')
+  try {
+    if (await exists(sshPath)) {
+      resultEl.innerHTML = `${sshPath} found`
+    } else {
+      resultEl.innerHTML = `${sshPath} NOT found`
+    }
+  } catch (e) {
+    resultEl.innerHTML = `Unable to check exist for ${sshPath} - ${e}`
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   let resultEl = document.querySelector("#result") as HTMLElement;
   var btn1 = document.getElementById("perform-test-1");
   if (btn1 != null) btn1.onclick = (async () => await runTest(true, resultEl));
-  var btn1 = document.getElementById("perform-test-2");
-  if (btn1 != null) btn1.onclick = (async () => await runTest(false, resultEl));
+  var btn2 = document.getElementById("perform-test-2");
+  if (btn2 != null) btn2.onclick = (async () => await runTest(false, resultEl));
+  var btn3 = document.getElementById("perform-test-3");
+  if (btn3 != null) btn3.onclick = (async () => await runSshTest(resultEl));
 });
